cfbdd66e77b5b7901cf422e14905c2c9066dbf98
devcomfort <kimdonghyun026@gmail.com>
2023-05-18 14:02:20 +0900

    comment : 주석 추가

--- working on/entrypicker.py
+++ working on/entrypicker.py
@@ -4,12 +4,14 @@ import pickle
 players = []
 selected_players = set()
 
+# TODO: 가급적이면 while 조건에서 선수 수를 제한하는 것이 아닌, while은 무한 반복으로 돌리고 반복문 내에서 경고를 주도록 처리하는 것이 사용자 경험 측면에서 더 좋음
 while len(selected_players) < 9:
     name: str = input("선수를 입력하세요 (선수 번호 또는 이름): ")
 
+    # -> 번호 반영 안 됨g
     # found_players = [player for player in player_dict.values() if player.name == name]
     found_players = list(filter(
-        # -> 이름 또는 번호가 같으면
+        # -> "이름 또는 번호가 같으면"
         lambda p: p.name == name or p.number == int(name),
         player_dict.values()
     ))
