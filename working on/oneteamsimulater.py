960635890468d879809a1753d9bd85e325b7b16b
devcomfort <kimdonghyun026@gmail.com>
2023-05-18 18:14:12 +0900

    fix

--- working on/oneteamsimulater.py
+++ working on/oneteamsimulater.py
@@ -1,7 +1,9 @@
+from typing import List, Union, Literal
+
 import random
 import pickle
 
-from classis import Player
+from classis import Player, Players
 
 
 # 이닝 후 베이스 상황 출력하는 함수
@@ -13,33 +15,30 @@ def basecall():
     print(f"홈: {home}")
     print()
 
-# Player 클래스 정의
-class Player:
-    def __init__(self, name, position, number):
-        self.name = name
-        self.position = position
-        self.number = number
-        self.selected = False
-
-    def __str__(self):
-        return f"이름: {self.name}, 포지션: {self.position}, 번호: {self.number}"
+# Player 클래스 정의부 제거 (의존성은 하나만)
+players: List[Player] = []  # entrypicker에서 선택된 선수 리스트
+cloned_players: List[Player] = []  # selected 요소가 초기화된 리스트
 
 # selected_players.pkl 파일에서 선수 정보 가져오기
-with open("selected_players.pkl", "rb") as file:
-    players = list(pickle.load(file))
+with open("players.pkl", "rb") as file:
+    pickled_players = pickle.load(file)  # 정상적으로 저장되었다면, list()를 사용하지 않아도 list로 저장됨
+
+    if type(pickled_players) == list:
+        players = Players.to_players(pickled_players)
+        for player in players:
+            player.selected = False
+            cloned_players.append(player)
 
 # 경기 진행
-inning_count = 1
-out_count = 0
-doosan = 0
+inning_count: int = 1
+out_count: int = 0
+doosan: int = 0
 
-# 선수들의 결과 기록 초기화
-player_results = {player: None for player in players}
 
 # 메인 게임 루프
 while out_count < 3:
-    print('--------------------------------'*5)
-    print('--------------------------------'*5)
+    print('-'*32*5)
+    print('-'*32*5)
     print(f"{inning_count}이닝 시작!")
     base1 = None
     base2 = None
@@ -47,21 +46,27 @@ while out_count < 3:
     home = None
 
     # 선수들 순환
-    while players:
-        selected_players = [player for player in players if player.selected]
+    # while players:  # players는 truthy 값 -> 무한 반복
+    random.shuffle(cloned_players)
+    for player in cloned_players:
+        # selected_players = list(filter(lambda player: player.selected, cloned_players))
         
-        if len(selected_players) == len(players):
-            break  # 모든 선수가 선택된 경우 게임 종료
+        # if len(selected_players) == len(cloned_players):
+        #     break  # 모든 선수가 선택된 경우 게임 종료
         
-        available_players = [player for player in players if not player.selected]
-        player = random.choice(available_players)
+        # available_players = list(filter(lambda player: not player.selected, cloned_players))
+    
+        # player = random.choice(available_players)
         player.selected = True
+        
         print(f"{player.name} 타석에 들어섰습니다.")
+        
         strikes = 0
 
         # 타격 루프
         while strikes < 3:
             result = random.choice(["스트라이크", "헛스윙", "치다", "홈런", "볼"])
+
             print(f"{player.name}: {result}")
 
             # 결과 처리
@@ -72,6 +77,7 @@ while out_count < 3:
                 print("볼!")
             elif result == "치다":
                 hit_result = random.choice(["아웃", "플라이아웃", "1루타", "2루타", "3루타"])
+                
                 print(f"{player.name}: {hit_result}")
 
                 if hit_result == "1루타":
