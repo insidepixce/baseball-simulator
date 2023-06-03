f90ff9ee77a4555be414b5e466ea84efc2f4847a
insidepixce <insidepixce@gmail.com>
2023-06-03 12:58:01 +0900

    타자 기록 페이지를 찾을 수 없다

---
+++ scrapping_batterscore.py
@@ -0,0 +1,39 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+# 웹 페이지에 접속하여 HTML 데이터 가져오기
+url = 'https://m.sports.naver.com/game/20230602OBKT02023/record'
+headers = {'User-Agent': 'Mozilla/5.0'}
+response = requests.get(url, headers=headers)
+
+# Response의 status code를 확인하여 요청이 정상적으로 이루어졌는지 확인
+if response.status_code != 200:
+    print(f"Request failed: status code {response.status_code}")
+else:
+    html = response.text
+
+    # BeautifulSoup을 사용하여 HTML 파싱
+    soup = BeautifulSoup(html, 'html.parser')
+
+    # 타자 기록 테이블 추출
+    table = soup.find('table', class_='record_table')
+
+    if table is not None:
+        # 테이블의 헤더 추출
+        headers = [th.text for th in table.select('thead th')]
+
+        # 테이블의 각 행 추출
+        rows = []
+        for tr in table.select('tbody tr'):
+            data = [td.text.strip() for td in tr.select('td')]
+            rows.append(data)
+
+        # DataFrame 생성
+        df = pd.DataFrame(rows, columns=headers)
+
+        # DataFrame을 엑셀 파일로 저장
+        df.to_excel('baseball_records.xlsx', index=False)
+        print('엑셀 파일로 저장되었습니다.')
+    else:
+        print('타자 기록 테이블을 찾을 수 없습니다.')
